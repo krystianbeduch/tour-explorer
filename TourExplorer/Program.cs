688f8773a9c1ad35291186f22de79e4cca1874b0
Krystian Beduch <beduch_krystian@o2.pl>
2024-06-07 18:54:27 +0200

    Remove log method from Program.cs

--- TourExplorer/Program.cs
+++ TourExplorer/Program.cs
@@ -9,23 +9,9 @@ namespace TourExplorer {
         /// </summary>
         [STAThread]
         static void Main() {
-            //try {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new WelcomeForm());
-            //}
-            //catch (Exception ex) {
-                //LogError(ex);
-                //MessageBox.Show("Wystąpił błąd. Szczegóły zostały zapisane w pliku error.log.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //}
-        }
-        static void LogError(Exception ex) {
-            string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "error.log");
-            using (StreamWriter writer = new StreamWriter(logFilePath, true)) {
-                writer.WriteLine($"{DateTime.Now}: {ex.Message}");
-                writer.WriteLine(ex.StackTrace);
-                writer.WriteLine();
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new WelcomeForm());
         }
     }
 }
