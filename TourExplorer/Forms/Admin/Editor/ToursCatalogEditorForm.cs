5c9d7878ee378ae6d63151790a39924fd4d177cb
Krystian Beduch <beduch_krystian@o2.pl>
2024-06-07 18:51:29 +0200

    delete Show/Hide from AddTour and DeleteTour options in ToursCatalogEditor

--- TourExplorer/Forms/Admin/Editor/ToursCatalogEditorForm.cs
+++ TourExplorer/Forms/Admin/Editor/ToursCatalogEditorForm.cs
@@ -35,17 +35,13 @@ namespace TourExplorer {
         private void buttonAddTourToCatalog_Click(object sender, EventArgs e) {
             // wybór 2. opcji - Dodawnie wycieczki do katalogu
             AddNewTourForm addNewTourForm = new AddNewTourForm();
-            Hide();
             addNewTourForm.ShowDialog();
-            Show();
         }
 
         private void buttonDeleteTourFromCatalog_Click(object sender, EventArgs e) {
             // wybór 3. opcji - Usuwanie wycieczki z katalogu
             DeleteTourForm deleteTourForm = new DeleteTourForm();
-            Hide();
             deleteTourForm.ShowDialog();
-            Show();
         }
 
         private void buttonExit_Click(object sender, EventArgs e) {
